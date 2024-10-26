b6aa5b09940e2b86eb7f2a030f1068c38bf0aa43
AreejAshfaq <areejch15222@gmail.com>
2024-10-26 04:31:10 -0700

    first commit

---
+++ main.py
@@ -0,0 +1,31 @@
+#string :is data type that store sequence of charcaters
+
+"""str1="i am areej"
+print(str1)#using back slash \n this is escap sequence character for ptint strung in next line
+str2="yes i am develpor .\n so contect with me if you need any type ofproject"
+print(str2)
+#conceteonation
+str1="areej"
+str2="Ashfaq"
+print(str1+str2)
+#length of str
+print(len(str1))
+print (len(str2))
+str4="zimal"
+print(type(str4))
+#indexing
+str="zimal Fatima"
+ch=str[1]
+print(str)"""
+#slicing
+"""str="i am social media infulancer"
+print(str[0:11])
+print(str[4:15])"""
+#negative index
+"""str="here i am"
+print(str[-7:-1])"""
+#string function
+#str.endwrite("er.")returen true when sunstring is end with substr
+str="i am a accountent at BDS"
+#print(str.endswith("BDS"))
+print(str.endswith("Bsc"))
